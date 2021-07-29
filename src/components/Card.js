23f37b1dc1538193d8f3bc31ab6d06ea0c949962
Yaniv Ben David <yanivbd@gmail.com>
2021-07-29 15:58:25 +0300

    Added split to expenses.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import "./Card.css";
 
 function Card(props) {
-  const classes = "class " + props.className;
+  const classes = "card " + props.className;
 
   return <div className={classes}>{props.children}</div>;
 }
